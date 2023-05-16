bfc9c199d50b7fc15da743e42dc49bde3eed001b
Alireza <arbor0711@gmail.com>
2023-05-16 21:06:11 +0200

    Add effect hook for fetching API data by click

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React, { useEffect, useState } from "react";
+import "./App.css";
 
 export default function App() {
   const [toggle, setToggle] = useState(false);
+  const [user, setUser] = useState({});
+  const [request, setRequest] = useState(false);
 
   const clickHandler = () => setToggle(!toggle);
 
+  const newUser = () => {
+    setRequest(!request);
+  };
+
   useEffect(() => {
-    document.title = toggle
-      ? "Welcome to Little Lemon"
-      : "Using the useEffect hook";
-  }, [toggle]);
+    fetch("https://randomuser.me/api/?results=1")
+      .then((res) => res.json())
+      .then((data) => setUser(data.results[0]));
+  }, [request]);
 
   return (
-    <div>
+    <div className="card">
       <h1>Using the useEffect hook</h1>
       <button onClick={clickHandler}>Toggle message</button>
       {toggle && <h2>Welcome to Little Lemon</h2>}
+      <button onClick={newUser}>Get New User</button>
+      <p>{user.email} </p>
     </div>
   );
 }
