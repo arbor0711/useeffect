7b4176334cf06c54f48b138dcf9ba50309a3901a
Alireza <arbor0711@gmail.com>
2023-05-16 23:28:55 +0200

    New useEffect component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,27 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import React from "react";
 
-export default function App() {
-  const [toggle, setToggle] = useState(false);
-  const [user, setUser] = useState({});
-  const [request, setRequest] = useState(false);
-  const [btcData, setBtcData] = useState({});
-
-  const clickHandler = () => setToggle(!toggle);
-
-  const newUser = () => {
-    setRequest(!request);
-  };
-
-  useEffect(() => {
-    fetch("https://randomuser.me/api/?results=1")
-      .then((res) => res.json())
-      .then((data) => setUser(data.results[0]));
-  }, [request]);
+function App() {
+  const [user, setUser] = React.useState([]);
 
   const fetchData = () => {
-    fetch("http://api.coindesk.com/v1/bpi/currentprice.json")
-      .then((res) => res.json())
-      .then((jsonData) => setBtcData(jsonData.bpi.USD))
-      .catch((e) => console.log(e));
+    fetch("https://randomuser.me/api/?results=1")
+      .then((response) => response.json())
+      .then((data) => setUser(data));
   };
 
-  useEffect(() => {
+  React.useEffect(() => {
     fetchData();
   }, []);
 
-  return (
-    <div className="card">
-      <div>
-        <h1>Using the useEffect hook</h1>
-        <button onClick={clickHandler}>Toggle message</button>
-        {toggle && <h2>Welcome to Little Lemon</h2>}
-      </div>
-      <div>
-        <button onClick={newUser}>Get New User</button>
-        {Object.keys(user).length > 0 ? (
-          <p>{user.email} </p>
-        ) : (
-          <h1>Data Pending...</h1>
-        )}
-      </div>
-      <div>
-        <h2>Current BTC/USD Data:</h2>
-        <p>Code: {btcData.code}</p>
-        <p>symbol: {btcData.symbol}</p>
-        <p>rate: {btcData.rate}</p>
-        <p>description: {btcData.description}</p>
-        <p>rate_float: {btcData.rate_float}</p>
-      </div>
+  return Object.keys(user).length > 0 ? (
+    <div style={{ padding: "40px" }}>
+      <h1>Customer data</h1>
+      <h2>Name: {user.results[0].name.first}</h2>
+      <img src={user.results[0].picture.large} alt="" />
     </div>
+  ) : (
+    <h1>Data pending...</h1>
   );
 }
+
+export default App;
